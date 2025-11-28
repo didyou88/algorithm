f30844b73e5527beb35a0c224f512205b09a4e35
SHIN JISU <81508062+didyou88@users.noreply.github.com>
2025-11-28 20:02:32 +0900

    [level 0] Title: 개미 군단, Time: 0.02 ms, Memory: 87.8 MB -BaekjoonHub

---
+++ 프로그래머스/0/120837. 개미 군단/개미 군단.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int solution(int hp) {
+        int answer = 0;
+        
+        answer += (int) hp / 5;
+        hp = (int) hp % 5;
+        
+        answer += (int) hp / 3;
+        answer += (int) hp % 3;
+        
+        return answer;
+    }
+}
