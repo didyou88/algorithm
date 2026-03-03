bcfd0a604fc87f9d9e79bab0ed136b69d13993ed
SHIN JISU <81508062+didyou88@users.noreply.github.com>
2026-03-03 22:42:05 +0900

    [level 3] Title: 입국심사, Time: 701.30 ms, Memory: 13.6 MB -BaekjoonHub

--- 프로그래머스/3/43238. 입국심사/입국심사.py
+++ 프로그래머스/3/43238. 입국심사/입국심사.py
@@ -1,23 +1,52 @@
 def solution(n, times):
-    times.sort()
+    # 초기화 - 리스트는 정렬되어야 한다
     ans = 0
-    left = 0
-    right = n * times[len(times)-1]
+    times.sort()
+    s = 0
+    e = times[len(times)-1] * n
     mid = 0
     
-    while left <= right:
-        mid = (left + right) // 2
+    while s <= e:
+        mid = (s+e) // 2
         checked_num = 0
         
+        # mid 시간 동안 몇 명이나 확인할 수 있는지 확인
         for time in times:
             checked_num += (mid // time)
-            
-        print(mid, checked_num)
+        
+        print(mid, checked_num, n)
         
         if checked_num < n:
-            left = mid + 1
+            s = mid + 1
         else:
-            right = mid - 1
+            e = mid - 1
             ans = mid
-    
+
     return ans
+
+    
+    
+    
+    
+#     times.sort()
+#     ans = 0
+#     left = 0
+#     right = n * times[len(times)-1]
+#     mid = 0
+    
+#     while left <= right:
+#         mid = (left + right) // 2
+#         checked_num = 0
+        
+#         for time in times:
+#             checked_num += (mid // time)
+            
+#         print(mid, checked_num)
+        
+#         if checked_num < n:
+#             left = mid + 1
+#         else:
+#             right = mid - 1
+#             ans = mid
+    
+#     return ans
